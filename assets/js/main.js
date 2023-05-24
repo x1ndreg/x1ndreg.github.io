b84758239b0ac851e2d28af9c0a1259b200a8905
x1ndreg <gvbsr55@gmail.com>
2023-05-24 20:58:28 +0800

    made alot of changes and revamp

--- assets/js/main.js
+++ assets/js/main.js
@@ -205,3 +205,26 @@ var typed = new Typed(".auto-typed", {
     backSpeed: 150,
     loop: true
 })
+
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("scroll-up");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+      scrollProgress.style.display = "grid";
+    } else {
+      scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+      document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#6e57e0 ${scrollValue}%, #191627 ${scrollValue}%)`;
+  };
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
+
+
