91aabbe1c1ff025d5d6a526a0ffb0fee2eb98751
x1ndreg <gvbsr55@gmail.com>
2023-05-27 10:35:04 +0800

    revised the preloader

---
+++ assets/js/preloader.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let counter = document.querySelector('.counter');
+    let loader = document.querySelector('.loader');
+    let preloader = document.querySelector('.preloader');
+    let count = 0;
+    let counterFunction = setInterval(() => {
+        if (count < 100) {
+            counter.textContent = `${count}%`;
+            loader.style.width = `${count}$`;
+            count++;
+        } else {
+            clearInterval(count);
+            fadeOut(preloader);
+        }
+    }, 20);
+
+    //fadeout
+    function fadeOut(element) {
+        element.style.opacity = 1;
+
+        (function fade() {
+            if ((element.style.opacity -= .1) < 0) {
+                element.style.display = 'none';
+            } else {
+                requestAnimationFrame(fade);
+            }
+        })();
+    }
+})
